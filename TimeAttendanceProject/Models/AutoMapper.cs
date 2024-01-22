17eb1bcd582a997fb2aea11594c36163798aa5c7
Refije <Refijefejzullahu02@gmail.com>
2024-01-22 16:14:19 +0100

    created automapper

---
+++ TimeAttendanceProject/Models/AutoMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using TimeAttendanceProject.DTOs;
+using TimeAttendanceProject.Models;
+
+
+namespace TimeAttendanceProject
+{
+ 
+        public class AutoMapper : Profile
+        {
+            public AutoMapper()
+            {
+                CreateMap<UserDTO, User>();
+                CreateMap<TasksDTO, Tasks>();
+                CreateMap<LoginRecordDTO, LoginRecord>();
+            }
+        }
+}
