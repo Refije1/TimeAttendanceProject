315b8a505ef3a4d1721ef7acd408dee176c3b4ba
Refije <Refijefejzullahu02@gmail.com>
2024-01-23 16:06:48 +0100

    LoginRecorder and Tasks crud done

---
+++ TimeAttendanceProject/Controllers/LoginRecordController.cs
@@ -0,0 +1,88 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TimeAttendanceProject.DataService;
+using TimeAttendanceProject.DTOs;
+using TimeAttendanceProject.Models;
+
+
+namespace TimeAttendanceProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginRecordController : Controller
+    {
+        private readonly TimeAttendanceContext _context;
+        private readonly IMapper _mapper;
+
+        public LoginRecordController(TimeAttendanceContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> AddLoginRecord([FromBody] LoginRecordDTO loginRecord)
+        {
+            if (loginRecord == null)
+            {
+                return BadRequest("Invalid user data");
+            }
+            var newLoginRecord = _mapper.Map<LoginRecord>(loginRecord);
+
+            _context.LoginRecords.Add(newLoginRecord);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetLoginRecord(int id)
+        {
+            var loginRecord = _context.LoginRecords.Find(id);
+
+            if (loginRecord  == null)
+            {
+                return NotFound();
+            }
+            return Ok(loginRecord);
+
+        }
+
+        [HttpPut("{update-id}")]
+        public async Task<IActionResult> UpdateLoginRecord(int id, [FromBody] LoginRecordDTO updatedLoginRecordDTO)
+        {
+            if (updatedLoginRecordDTO == null)
+            {
+                return BadRequest("Invalid data or mismatched id");
+            }
+            var existingLoginRecord = _context.LoginRecords.Find(id);
+            if (existingLoginRecord == null)
+            {
+                return NotFound();
+
+            }
+
+            _mapper.Map(updatedLoginRecordDTO, existingLoginRecord);
+
+            _context.LoginRecords.Update(existingLoginRecord);
+            await _context.SaveChangesAsync();
+            return Ok(existingLoginRecord);
+
+        }
+
+        [HttpDelete("delete-id")]
+        public async Task<IActionResult> DeleteLoginRecord(int id)
+        {
+            var loginRecord = _context.LoginRecords.Find(id);
+
+            if (loginRecord == null)
+            {
+                return NotFound();
+            }
+            _context.LoginRecords.Remove(loginRecord);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
+        }
+    }
+}
---
+++ TimeAttendanceProject/Controllers/TasksController.cs
@@ -0,0 +1,85 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using TimeAttendanceProject.DataService;
+using TimeAttendanceProject.DTOs;
+using TimeAttendanceProject.Models;
+
+
+namespace TimeAttendanceProject.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TasksController : Controller
+    {
+        private readonly TimeAttendanceContext _context;
+        private readonly IMapper _mapper;
+
+        public TasksController(TimeAttendanceContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> AddTasks([FromBody] TasksDTO tasks)
+        {
+            if (tasks == null)
+            {
+                return BadRequest("Invalid user data");
+            }
+            var newTasks = _mapper.Map<Tasks>(tasks);
+
+            _context.Tasks.Add(newTasks);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTasks(int id)
+        {
+            var tasks = _context.Tasks.Find(id);
+
+            if (tasks == null)
+            {
+                return NotFound();
+            }
+            return Ok(tasks);
+
+        }
+
+        [HttpPut("{update-id}")]
+        public async Task<IActionResult> UpdateTasks(int id, [FromBody] TasksDTO updatedTasksDTO)
+        {
+            if (updatedTasksDTO == null) {
+                return BadRequest("Invalid data or mismatched id");
+            }   
+            var existingTasks = _context.Tasks.Find(id);
+            if (existingTasks == null) {
+                return NotFound();
+                        
+            }
+
+            _mapper.Map(updatedTasksDTO, existingTasks);
+
+            _context.Tasks.Update(existingTasks);
+            await _context.SaveChangesAsync();
+            return Ok(existingTasks);
+
+        }
+
+        [HttpDelete("delete-id")]
+        public async Task<IActionResult> DeleteTasks(int id)
+        {
+            var tasks = _context.Tasks.Find(id);
+
+            if (tasks == null) {
+                return NotFound();
+                 }
+            _context.Tasks.Remove(tasks);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+
+        }
+    }
+}
